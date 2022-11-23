13f3bfb8c6cf68e5858bf6af1a1df938badbbb77
vincent d warmerdam <vincentwarmerdam@gmail.com>
2022-11-23 13:12:41 +0000

    span-and-textcat-added

---
+++ tutorials/span-and-textcat/README.md
@@ -0,0 +1,11 @@
+# Annotating Chatbot Logs
+
+This recipe combines spancat annotation with textcat annotation, which is a great combo when annotating chatbot logs. 
+
+![](demo.png)
+
+You can run the recipe via; 
+
+```
+python -m prodigy span-and-textcat bot-demo en examples.jsonl -F recipe.py
+```
---
+++ tutorials/span-and-textcat/recipe.py
@@ -0,0 +1,54 @@
+import spacy
+import prodigy 
+from prodigy.components.preprocess import add_tokens
+from prodigy.components.loaders import JSONL
+
+
+@prodigy.recipe(
+    "span-and-textcat",
+    dataset=("Dataset to save annotations into", "positional", None, str),
+    lang=("Language to use", "positional", None, str),
+    file_in=("Path to examples.jsonl file", "positional", None, str)
+)
+def custom_recipe(dataset, lang, file_in):
+    span_labels = ["product", "amount", "size", "type", "topping"]
+    textcat_labels = ["greet", "inform", "purchase", "confirm"]
+
+    def add_options(stream):
+        for ex in stream:
+            ex['options'] = [
+                {"id": lab, "text": lab} for lab in textcat_labels
+            ]
+            yield ex
+
+    nlp = spacy.blank(lang)
+    stream = JSONL(file_in)
+    stream = add_tokens(nlp, stream, use_chars=None)
+
+    stream = add_options(stream)
+    blocks = [
+        {"view_id": "spans_manual"},
+        {"view_id": "choice", "text": None},
+    ]
+    return {
+        "view_id": "blocks",  # Annotation interface to use
+        "dataset": dataset,  # Name of dataset to save annotations
+        "stream": stream,  # Incoming stream of examples
+        "config": {  # Additional config settings, mostly for app UI
+            "lang": nlp.lang,
+            "labels": span_labels,
+            "blocks": blocks,
+            "keymap_by_label": {
+                "0": "q", 
+                "1": "w", 
+                "2": "e", 
+                "3": "r", 
+                "product": "1", 
+                "amount": "2",
+                "size": "3",
+                "type": "4",
+                "topping": "5" 
+            },
+            "choice_style": "multiple"
+        },
+    }
